4109d2292cb876d5c4dcc581018538bae466ed37
mvnribeiro <marvynribeiro@gmail.com>
2025-02-01 02:10:37 -0300

    feat(useWordDetails): add word details cache

--- hooks/useWordDetails.ts
+++ hooks/useWordDetails.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const useWordDetails = (word: string | string[] | undefined) => {
   const [details, setDetails] = useState<any>(null)
@@ -7,12 +8,19 @@ export const useWordDetails = (word: string | string[] | undefined) => {
 
   useEffect(() => {
     if (!word) return
+    const cacheKey = `${word}-details`
 
     const fetchWordDetails = async () => {
       setLoading(true)
       setError(null)
 
       try {
+        const cacheData = await AsyncStorage.getItem(cacheKey)
+        if (cacheData) {
+          setDetails(JSON.parse(cacheData))
+          setLoading(false)
+          return
+        }
         const response = await fetch(
           `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
         )
@@ -20,7 +28,10 @@ export const useWordDetails = (word: string | string[] | undefined) => {
         if (!response.ok) throw new Error('Word not found')
 
         const data = await response.json()
-        setDetails(data[0])
+        const detailsData = data[0]
+
+        await AsyncStorage.setItem(cacheKey, JSON.stringify(detailsData))
+        setDetails(detailsData)
       } catch (err) {
         setError(err instanceof Error ? err.message : 'Failed to fetch details')
       } finally {
