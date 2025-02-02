94b58fe948f7f14773f662de91295ab6164c96f8
mvnribeiro <marvynribeiro@gmail.com>
2025-02-02 07:14:39 -0300

    chore(firebaseAuth): fix login warning

--- firebase/firebaseAuth.js
+++ firebase/firebaseAuth.js
@@ -1,13 +1,17 @@
 import {
-  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
   onAuthStateChanged,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   User
 } from 'firebase/auth'
 import app from './firebaseConfig'
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-const auth = getAuth(app)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+})
 
 export {
   auth,
